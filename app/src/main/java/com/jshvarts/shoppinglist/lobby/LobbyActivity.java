150bc4e0c72c6d5e7ddb0978d11d022fa6c0fa56
James Shvarts <jshvarts@gmail.com>
2017-07-29 01:08:46 -0400

    add toolbar and fab

--- app/src/main/java/com/jshvarts/shoppinglist/lobby/LobbyActivity.java
+++ app/src/main/java/com/jshvarts/shoppinglist/lobby/LobbyActivity.java
@@ -2,8 +2,10 @@ package com.jshvarts.shoppinglist.lobby;
 
 import android.arch.lifecycle.LifecycleActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.jshvarts.shoppinglist.R;
 import com.jshvarts.shoppinglist.lobby.fragments.AddShoppingListItemFragment;
@@ -13,19 +15,18 @@ import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 import dagger.android.AndroidInjection;
 import dagger.android.AndroidInjector;
 import dagger.android.DispatchingAndroidInjector;
 import dagger.android.support.HasSupportFragmentInjector;
 
-public class LobbyActivity extends LifecycleActivity implements HasSupportFragmentInjector {
+public class LobbyActivity extends AppCompatActivity implements HasSupportFragmentInjector {
 
     @Inject
     DispatchingAndroidInjector<Fragment> fragmentDispatchingAndroidInjector;
 
-    @BindView(R.id.create_shopping_list_item_button)
-    Button addShoppingListItemButton;
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,16 +36,18 @@ public class LobbyActivity extends LifecycleActivity implements HasSupportFragme
 
         ButterKnife.bind(this);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        fab.setOnClickListener(v -> {
+            Fragment addShoppingListItemFragment = new AddShoppingListItemFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.lobby_root_view, addShoppingListItemFragment).commit();
+        });
+
         Fragment shoppingListFragment = new ShoppingListFragment();
         getSupportFragmentManager().beginTransaction()
-                .add(R.id.lobby_root_view, shoppingListFragment).commit();
-    }
-
-    @OnClick(R.id.create_shopping_list_item_button)
-    void onAddShoppingListItemButtonClicked() {
-        Fragment addShoppingListItemFragment = new AddShoppingListItemFragment();
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.lobby_root_view, addShoppingListItemFragment).commit();
+                .replace(R.id.lobby_root_view, shoppingListFragment).commit();
     }
 
     @Override
