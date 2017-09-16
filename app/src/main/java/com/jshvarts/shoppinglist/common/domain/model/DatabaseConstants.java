5c68ff0c691e8c66e62cb616e5b95d02f0488a74
James Shvarts <jshvarts@gmail.com>
2017-09-16 16:19:17 -0400

    integrate travis ci

--- app/src/main/java/com/jshvarts/shoppinglist/common/domain/model/DatabaseConstants.java
+++ app/src/main/java/com/jshvarts/shoppinglist/common/domain/model/DatabaseConstants.java
@@ -1,5 +1,5 @@
 package com.jshvarts.shoppinglist.common.domain.model;
 
 public class DatabaseConstants {
-    public static String DEFAULT_SHOPPING_LIST_ID = "-KqPzcShiT8YJURTvDbY";
+    public static String DEFAULT_SHOPPING_LIST_ID = "-Ks4AYWRbtHXRd4dUJl2";
 }
